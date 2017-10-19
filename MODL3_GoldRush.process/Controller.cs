1440e02fef1220779c5b0ba96dcaf6d6c90da222
Arcaev <Jorrit-93@hotmail.com>
2017-10-19 15:21:24 +0200

    Added Level1.txt

--- MODL3_GoldRush.process/Controller.cs
+++ MODL3_GoldRush.process/Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using MODL3_GoldRush.presentation;
 
 namespace MODL3_GoldRush.process
@@ -16,6 +17,19 @@ namespace MODL3_GoldRush.process
 		{
 			inView = new InputView();
 			outView = new OutputView();
+			LoadMaze(); //temp
+		}
+
+		public void LoadMaze()
+		{
+			string[] lines;
+			int nr = 1; //temp
+			lines = File.ReadAllLines(@Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\Level" + nr + ".txt");
+			foreach(string s in lines)
+			{
+				Console.WriteLine(s);
+			}
+			Console.Read();
 		}
 	}
 }
